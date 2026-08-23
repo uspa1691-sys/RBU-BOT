d097b1080e26bc509a04fe8a1c9c964fda8b02db
uspa1691-sys <uspa1691@gmail.com>
2026-08-23 09:33:58 +0500

    Update bot (1).py

--- bot (1).py
+++ bot (1).py
@@ -16,7 +16,7 @@ from telegram.ext import (
 # =========================================================
 
 # Token endi kodda emas — Fly.io secrets orqali beriladi
-BOT_TOKEN = "8287856383:AAF2mKHyEHqlL01LRyRGZ6IgmIGYRjyr4L0"
+BOT_TOKEN = "7873626779:AAFs4bJjzd-7d3JiLP9D7vhARmi-w2KQ7WY"
 
 if not BOT_TOKEN:
     raise RuntimeError(
